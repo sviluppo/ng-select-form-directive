55ff375e7f245d804c789a030b1347bbfc420c34
alberto <alby.tav@gmail.com>
2022-03-12 16:08:59 +0100

    solution

--- src/app/custom-ng-select/custom-ng-select.component.ts
+++ src/app/custom-ng-select/custom-ng-select.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {Component, EventEmitter, Injector, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl} from "@angular/forms";
+import {NgSelectComponent} from "@ng-select/ng-select";
 
 @Component({
   selector: 'custom-ng-select',
@@ -15,6 +16,7 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 export class CustomNgSelectComponent implements ControlValueAccessor {
   private onTouched!: Function;
   private onChanged!: Function;
+  @ViewChild('ngSelect', { static: true }) ngSelect!: NgSelectComponent;
   @Input() observable$!: any;
   @Input() observableInput$!: any;
   @Input() loading!: boolean;
@@ -23,9 +25,17 @@ export class CustomNgSelectComponent implements ControlValueAccessor {
   @Input() label: string = '';
   @Output() onSearch = new EventEmitter();
   selected: any;
+  ngControl!: NgControl;
+
+  constructor(public injector: Injector) {}
 
   ngOnInit(){
     this.onSearch.emit();
+    this.ngControl = this.injector.get(NgControl, undefined);
+  }
+
+  ngAfterViewInit(){
+    this.setDefaultValue();
   }
 
   select(value: string) {
@@ -45,5 +55,12 @@ export class CustomNgSelectComponent implements ControlValueAccessor {
 
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
+  }
+
+  setDefaultValue(){
+    const value = this.ngControl?.control?.value[this.bindValue];
+    if(value){
+      this.ngSelect.writeValue(value);
+    }
   }
 }
